a9d5dd880dae40bd9669855e35cf492c8e4eab68
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-07-02 12:34:45 +0700

    Set up Authentication
    - Whenever someone login to the page, they are allowed to access home page, or else they will be navigated to login.
    - If someone enter wrong address, they will be meet error page.

---
+++ client/src/context/AuthProvider.jsx
@@ -0,0 +1,47 @@
+import React, { 
+  createContext, 
+  useEffect, 
+  useState,
+} from 'react';
+import {
+  getAuth,
+} from 'firebase/auth';
+import {
+  useNavigate,
+} from 'react-router-dom';
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  const auth = getAuth();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribed = auth.onIdTokenChanged((user) => {
+      console.log('[From client/context/AuthProvider]', {user});
+      if (user?.uid) {
+        setUser(user);
+        localStorage.setItem('accessToken', user.accessToken);
+        return;
+      }
+
+      setUser({});
+      localStorage.clear();
+      navigate('/login');
+    });
+
+    return () => {
+      unsubscribed();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,23 +1,36 @@
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
+import AuthProvider from "../context/AuthProvider";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AuthLayout = () => {
-  return <Outlet /> 
+  return (
+    <AuthProvider>
+      <Outlet />
+    </AuthProvider>
+  ); 
 }
 
 export default createBrowserRouter([
   {
     element: <AuthLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         element: <Login />,
         path: '/login',
       },
       {
-        element: <Home />,
-        path: '/',
-      }
-    ]
-  }
-])
+        element: <ProtectedRoute />,
+        children: [
+          {
+            element: <Home />,
+            path: '/',
+          }
+        ],
+      },   
+    ],
+  },
+]);
