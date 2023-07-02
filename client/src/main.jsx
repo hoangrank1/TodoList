5fb31e4f13cfed273bfb27a38e19305ed2ecdaf2
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-07-02 12:32:47 +0700

    Set up firebase

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,16 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import router from './router';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
+import { Container } from '@mui/system';
+import './firebase/config';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <Container maxWidth='lg' sx={{ textAlign: 'center', marginTop: '50px' }}>
+    <RouterProvider router={router} />
+  </Container>
 )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { 
+  useContext, 
+} from 'react';
+import {
+  Button,
+  Typography,
+} from '@mui/material';
+import { 
+  GoogleAuthProvider,
+  signInWithPopup,
+  getAuth,
+} from 'firebase/auth';
+import { 
+  AuthContext, 
+} from '../context/AuthProvider';
+import { 
+  useNavigate, 
+} from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  
+  const auth = new getAuth();
+  const { 
+    user, 
+  } = useContext(AuthContext);
+
+  const handleLoginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    
+    const res = await signInWithPopup(auth, provider);
+
+    console.log('[From client/pages/Login]', { res });
+  }
+
+  if (user?.uid) {
+    navigate('/');
+    return
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <Typography variant='h5' sx={{ marginBottom: '10px' }}>
+        Welcome to Note App
+      </Typography>
+      <Button variant='outlined' onClick={handleLoginWithGoogle}>
+        Login with Google
+      </Button>
+    </div>
   )
 }
