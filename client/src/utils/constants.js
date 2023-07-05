f8dfe6b0bc57a623d41cce32445d218b8b2b841d
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-07-05 08:51:50 +0700

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = 'https://todolist-server-tnfh.onrender.com';
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://https://todolist-server-tnfh.onrender.com';
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://todolist-server-tnfh.onrender.com/';
